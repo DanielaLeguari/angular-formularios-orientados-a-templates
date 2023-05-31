bf78934da7be67f7410da317013e55aba762ea2d
Daniela Leguari <danielaleguari@outlook.com>
2023-05-31 11:00:04 -0300

    configuração da diretiva

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,7 +1,13 @@
 import { Directive } from '@angular/core';
+import { NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: '[appMaiorIdade]',
+  providers:[{
+    provide: NG_VALIDATORS,
+    useExisting: MaiorIdadeDirective,
+    multi: true
+  }]
 })
 export class MaiorIdadeDirective {
 
